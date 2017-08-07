f4de98bc2207dad865bb76b15c0f53b5987171ca
medis <aubuka@gmail.com>
2017-08-07 23:47:07 +0100

    first commit

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Setup complete</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <meta name="description" value="Violeta Skaya - singer and song writer">
+        <title>Violeta Skya</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+
+    </head>
+    <body>
+        <div id="app">
+            <app></app>
+        </div>
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
