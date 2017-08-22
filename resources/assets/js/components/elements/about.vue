eed1186e8ffe6fa0133fd5e73c923833143f11ee
medis <aubuka@gmail.com>
2017-08-22 21:40:59 +0100

    change short bio

--- resources/assets/js/components/elements/about.vue
+++ resources/assets/js/components/elements/about.vue
@@ -4,8 +4,8 @@
     <router-link to="/bio" title="read all bio" class="more is-pulled-right is-size-7">FULL BIO</router-link>
     <h2 class="title is-4">About Violeta Skya</h2>
     <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet diam volutpat, volutpat metus in, egestas quam. Integer accumsan efficitur viverra.</p>
-      <p>vitae nunc fermentum, ullamcorper mi sed, hendrerit tortor. Proin luctus, felis non convallis ornare, velit ligula ultricies leo, et porttitor nulla libero a dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus at sem quis ipsum rutrum iaculis vitae nec ligula. Curabitur et erat vel neque imperdiet scelerisque.</p>
+      <p>Violeta Skya is known for her talents as a songwriter, topliner and memorable 'hook' maker. As a lyricist she emphasises the power of feeling. A story behind lyrics is important for her and she puts a new message in every song.</p>
+      <p>Emotional, often with a dark and edgy vibe, her songs and her versatile vocals leave audiences in the astonished state.</p>
     </div>
   </section>
 
