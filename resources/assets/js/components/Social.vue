50a60f810410d89ad67dbaed0a41edb9a541246d
medis <aubuka@gmail.com>
2017-11-20 22:37:43 +0000

    update itunes link

--- resources/assets/js/components/Social.vue
+++ resources/assets/js/components/Social.vue
@@ -4,7 +4,7 @@
       <li><a class="icon instagram" href="https://www.instagram.com/violetaskya/" target="_blank"><div class="bg"></div><i class="fa fa-instagram"></i></a></li>
       <li><a class="icon facebook" href="https://www.facebook.com/violetaskya/" target="_blank"><div class="bg"></div><i class="fa fa-facebook"></i></a></li>
       <li><a class="icon twitter" href="https://twitter.com/VioletaSkya" target="_blank"><div class="bg"></div><i class="fa fa-twitter"></i></a></li>
-      <li><a class="icon apple" href="#" target="_blank"><div class="bg"></div><i class="fa fa-apple"></i></a></li>
+      <li><a class="icon apple" href="https://itunes.apple.com/us/artist/violeta-skya/1273425660" target="_blank"><div class="bg"></div><i class="fa fa-apple"></i></a></li>
       <li><a class="icon spotify" href="https://open.spotify.com/artist/37ejdU331mtt2i5obvvWsz" target="_blank"><div class="bg"></div><i class="fa fa-spotify"></i></a></li>
       <li><a class="icon soundcloud" href="https://soundcloud.com/violeta-brodovskaja" target="_blank"><div class="bg"></div><i class="fa fa-soundcloud"></i></a></li>
     </ul>
