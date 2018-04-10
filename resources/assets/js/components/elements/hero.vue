a363307ba419356f43fc260a9f95c27ad2ef9569
medis <aubuka@gmail.com>
2018-04-10 23:06:36 +0100

    update hero, remove h1

--- resources/assets/js/components/elements/hero.vue
+++ resources/assets/js/components/elements/hero.vue
@@ -1,12 +1,4 @@
 <template>
   <section class="hero is-fullheight is-bold">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Violeta Skya
-        </h1>
-      </div>
-    </div>
-    <h2>Singer & Songwriter</h2>
   </section>
 </template>
